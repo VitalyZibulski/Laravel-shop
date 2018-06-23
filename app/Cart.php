dbf7c19ab4243522e85a56f438ffe75a0f5b47f5
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-06-23 14:09:35 +0300

    add cart functions

---
+++ app/Cart.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App;
+
+
+class Cart
+{
+	public $items;
+	public $totalQuantity;
+	public $totalPrice;
+
+	/**
+	 * Cart constructor.
+	 */
+	public function __construct($prevCart)
+	{
+		if($prevCart !=null){
+			$this->items = $prevCart->items;
+			$this->totalQuantity= $prevCart->items;
+			$this->totalPrice = $prevCart->items;
+		}
+		else {
+			$this->items = [];
+			$this->totalQuantity = 0;
+			$this->totalPrice = 0;
+		}
+	}
+
+	public function addItem($id, $product)
+	{
+		$price = (int)str_replace("$","", $product->price);
+		//the item already exists
+		if(array_key_exists($id,$this->items)){
+			$productToAdd = $this->items[$id];
+			$productToAdd['quantity']++;
+		} else {
+			$productToAdd = ['quantity' => 1, 'price' => $price, 'data' => $product];
+		}
+
+		$this->items[$id] = $productToAdd;
+		$this->totalQuantity++;
+		$this->totalPrice = $this->totalPrice + $price;
+	}
+}
